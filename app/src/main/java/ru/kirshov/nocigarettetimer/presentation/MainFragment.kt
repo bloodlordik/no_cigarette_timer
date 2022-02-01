959393b80f34fdb420e0e5b544bce3b92ca298b2
Roma Kirshiv <bloodlordik@yandex.ru>
2022-02-01 21:39:13 +0700

    Initial commit

---
+++ app/src/main/java/ru/kirshov/nocigarettetimer/presentation/MainFragment.kt
@@ -0,0 +1,7 @@
+package ru.kirshov.nocigarettetimer.presentation
+
+import androidx.fragment.app.Fragment
+import ru.kirshov.nocigarettetimer.R
+
+class MainFragment:Fragment(R.layout.fragment_main) {
+}
